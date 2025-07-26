549ccfbb62cf58b76bf17d7a1db103c6edbe469d
wikixen <bennylopezhernandez0@gmail.com>
2025-07-25 23:26:33 -0400

    finished day 1 p1

--- day1/day1_p1.c
+++ day1/day1_p1.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+void quicksort(int array[], int start, int end);
+int partition(int array[], int start, int end);
+void swap(int *a, int *b);
+
 int main(void) {
   // Open & read file contents into file array
   FILE *fp = fopen("input.txt", "r");
@@ -25,16 +29,73 @@ int main(void) {
     token = strtok(NULL, delimit);
     i++;
   }
+  i = 0;
 
   // Split nums into two separate arr
   int list1[1000] = {0};
   int list2[1000] = {0};
-  for (size_t j = 0; j < sizeof nums; j++)
+  int k = 0;
+  for (size_t j = 0; j < (sizeof(nums)/sizeof(nums[0])); j++)
   {
     if (j%2==0)
     {
-      list1[j] = nums[j];
+      list1[i] = nums[j];
+      i++;
+      // printf("%d\t", nums[j]);
+    } 
+    else if(j%2==1) 
+    {
+      list2[k] = nums[j];
+      k++;
+      // printf("%d\n", nums[j]);
     }
   }
-  
+
+  // Sorting
+  quicksort(list1, 0, (sizeof(list1) / sizeof(list1[0])) - 1);
+  quicksort(list2, 0, (sizeof(list2) / sizeof(list2[0])) - 1);
+
+  int sum = 0;
+  for (size_t j = 0; j < 1000; j++)
+  {
+    sum += abs(list1[j] - list2[j]);
+  }
+  printf("%d\n",sum);
+}
+
+void quicksort(int array[], int start, int end)
+{
+  if (start < end)
+  {
+    int pivot = partition(array, start, end);
+
+    quicksort(array, start, pivot - 1);
+    quicksort(array, pivot + 1, end);
+  }
+}
+
+int partition(int array[], int start, int end)
+{
+  int pivot = array[end];
+  int i = start - 1;
+
+  for (size_t j = start; j < end; j++)
+  {
+    if (array[j]<pivot)
+    {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+
+  i++;
+  swap(&array[i], &array[end]);
+  return i;
+}
+
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
